ba0f62a7e1870cb3cebfa1b46519602e783d86b2
Muhammad Suleman <leodeveloper@gmail.com>
2018-10-12 17:17:35 +0100

    First Commit

---
+++ AggregateRootAndIterativeDesignPattern/EmployeeAddress.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregateRootAndIterativeDesignPattern
+{
+    /// <summary>
+    /// Aggregate row Pattern, always go throw from root, list make it private
+    /// Iterative pattern, always return IEnumerable
+    /// </summary>
+    public class EmployeeAddress
+    {
+        public EmployeeAddress()
+        {
+            this._addresses = new List<string>();
+        }
+
+        private IList<string> _addresses { get; set; }
+
+        public void Add(string address)
+        {
+            this._addresses.Add(address);
+        }
+
+        public IEnumerable<string> Addresses // Step 3 :- To browse use enumerator
+        {
+            get { return _addresses; }
+        }
+
+
+    }
+}
---
+++ AggregateRootAndIterativeDesignPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AggregateRootAndIterativeDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeAddress employeeAddress = new EmployeeAddress();
+            employeeAddress.Add("123 Road");
+            employeeAddress.Add("1234 Road");
+            employeeAddress.Add("1235 Road");
+            employeeAddress.Add("1236 Road");
+
+            foreach (var address in employeeAddress.Addresses)
+            {
+                Console.WriteLine(address);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
